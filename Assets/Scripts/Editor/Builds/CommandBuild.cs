1f45f7f7917e1cfceba4fd511171297eca47921a
WeiJian Tan <tan.weijian@foxmail.com>
2021-06-28 23:43:20 +0800

    整理一下编辑器代码

---
+++ Assets/Scripts/Editor/Builds/CommandBuild.cs
@@ -0,0 +1,12 @@
+using UnityEditor;
+
+namespace Game.Editor
+{
+    public static class CommandBuild
+    {
+        [MenuItem("Build Tools/Start Build Full")]
+        public static void StartBuildFull()
+        {
+        }
+    }
+}
