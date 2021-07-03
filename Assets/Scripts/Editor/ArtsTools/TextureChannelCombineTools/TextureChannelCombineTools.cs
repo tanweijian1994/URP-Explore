b38f206e29d0d7f69b4ef8b588a2cff63c0892b0
WeiJian Tan <tan.weijian@foxmail.com>
2021-07-04 01:20:47 +0800

    整理一下设置

--- Assets/Scripts/Editor/ArtsTools/TextureChannelCombineTools/TextureChannelCombineTools.cs
+++ Assets/Scripts/Editor/ArtsTools/TextureChannelCombineTools/TextureChannelCombineTools.cs
@@ -1,7 +1,5 @@
 using UnityEditor;
 using UnityEngine;
-using UnityEngine.UIElements;
-using UnityEditor.UIElements;
 
 public class TextureChannelCombineTools : EditorWindow
 {
@@ -10,27 +8,5 @@ public class TextureChannelCombineTools : EditorWindow
     {
         TextureChannelCombineTools wnd = GetWindow<TextureChannelCombineTools>();
         wnd.titleContent = new GUIContent("TextureChannelCombineTools");
-    }
-
-    public void CreateGUI()
-    {
-        // Each editor window contains a root VisualElement object
-        VisualElement root = rootVisualElement;
-
-        // VisualElements objects can contain other VisualElement following a tree hierarchy.
-        VisualElement label = new Label("Hello World! From C#");
-        root.Add(label);
-
-        // Import UXML
-        var visualTree = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/Scripts/Editor/Tools/Arts/TextureChannelCombineTools/TextureChannelCombineTools.uxml");
-        VisualElement labelFromUXML = visualTree.Instantiate();
-        root.Add(labelFromUXML);
-
-        // A stylesheet can be added to a VisualElement.
-        // The style will be applied to the VisualElement and all of its children.
-        var styleSheet = AssetDatabase.LoadAssetAtPath<StyleSheet>("Assets/Scripts/Editor/Tools/Arts/TextureChannelCombineTools/TextureChannelCombineTools.uss");
-        VisualElement labelWithStyle = new Label("Hello World! With Style");
-        labelWithStyle.styleSheets.Add(styleSheet);
-        root.Add(labelWithStyle);
     }
 }
